22eba0ad1fcee806c9de6452f4943d27b657dd9f
grzeg <grzegorz.niespodzianek@gmail.com>
2026-02-17 00:47:53 +0100

    devices auth

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from enum import IntEnum
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, status, HTTPException
 from pydantic import BaseModel
+from uuid import uuid4
+import redis
 
 class Symbol(IntEnum):
     DOT = 0
     DASH = 1
 
-class MessageRequest(BaseModel):
+class AuthInfo(BaseModel):
     device_uid: str
+    auth_token: str
+
+
+class MessageRequest(BaseModel):
+    auth: AuthInfo
     symbol: Symbol
 
+
 app = FastAPI()
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 
 class ConnectionManager:
@@ -41,5 +52,20 @@ async def websocket_endpoint(websocket: WebSocket):
 
 @app.post("/send_message")
 async def send_message(request: MessageRequest):
+    cached_auth_token = redis_client.get(request.auth.device_uid)
+    if not cached_auth_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Device not registered")
+    
+    if cached_auth_token != request.auth.auth_token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid auth token")
+
     await manager.broadcast({"symbol": request.symbol.name, "value": request.symbol.value})
     return {"status": "Message broadcasted"}
+
+@app.get("/register_device")
+async def register_device():
+    device_uid = str(uuid4())
+    auth_token = str(uuid4())
+    redis_client.set(device_uid, auth_token, ex=1800)
+    return {"device_uid": device_uid, "auth_token": auth_token}
